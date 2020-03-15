2842e3c885dd44c70190d323850d83b22cf95ff3
Devin Trejo <dtrejod@users.noreply.github.com>
2020-03-15 10:31:21 -0400

    Misc improvements.
    Add support for video tag extraction. Handle collision by date by
    appending "-0X" to files. Attempts to de-duplicate pictures by comparing
    picture contents.

--- exifsort.sh
+++ exifsort.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
-# https://github.com/mb243/exifsort
 #
-function varbles() {
-
+#
 # The following are the only settings you should need to change:
 #
 # TS_AS_FILENAME: This can help eliminate duplicate images during sorting.
 # TRUE: File will be renamed to the Unix timestamp and its extension.
 # FALSE (any non-TRUE value): Filename is unchanged.
-
-export TS_AS_FILENAME=TRUE
+TS_AS_FILENAME=TRUE
 #
 # USE_LMDATE: If this is TRUE, images without EXIF data will have their Last Modified file
 # timestamp used as a fallback. If FALSE, images without EXIF data are put in noexif/ for
 # manual sorting.
 # Valid options are "TRUE" or anything else (assumes FALSE). FIXME: Restrict to TRUE/FALSE
 #
-export USE_LMDATE=TRUE
+USE_LMDATE=FALSE
 #
 # USE_FILE_EXT: The following option is here as a compatibility option as well as a bugfix.
 # If this is set to TRUE, files are identified using FILE's magic, and the extension
 # is set accordingly. If FALSE (or any other value), file extension is left as-is.
 # CAUTION: If set to TRUE, extensions may be changed to values you do not expect.
-# See the manual page for file(1) to understand how this works. Jeff says on my system
-#invalid file extensions are ignored and this only checks capitalisation
+# See the manual page for file(1) to understand how this works.
+# NOTE: This option is only honored if TS_AS_FILENAME is TRUE.
 #
-export USE_FILE_EXT=TRUE
+USE_FILE_EXT=TRUE
 #
 # JPEG_TO_JPG: The following option is here for personal preference. If TRUE, this will
 # cause .jpg to be used instead of .jpeg as the file extension. If FALSE (or any other
-# value) .jpeg is used instead.
+# value) .jpeg is used instead. This is only used if USE_FILE_EXT is TRUE and used.
 #
-export JPEG_TO_JPG=TRUE
+JPEG_TO_JPG=TRUE
 #
 #
 # The following is an array of filetypes that we intend to locate using find.
@@ -40,66 +37,18 @@ export JPEG_TO_JPG=TRUE
 # Extensions are matched case-insensitive. *.jpg is treated the same as *.JPG, etc.
 # Can handle any file type; not just EXIF-enabled file types. See USE_LMDATE above.
 #
-FILETYPES=("*.jpg" "*.jpeg" "*.png" "*.tif" "*.tiff" "*.gif" "*.xcf")
+FILETYPES=("*.jpg" "*.jpeg" "*.png" "*.tif" "*.tiff" "*.gif" "*.xcf" "*.mp4" "*.avi" "*.mov")
 #
-# Optional: Prefix of new top-level directory to move sorted photos to.
-# Can be a relative pathspec, but an # absolute pathspec is recommended.
+# Optional: Prefix of new top-level directory to kjmove sorted photos to.
+# if you use MOVETO, it MUST have a trailing slash! Can be a relative pathspec, but an
+# absolute pathspec is recommended.
+# FIXME: Gracefully handle unavailable destinations, non-trailing slash, etc.
 #
-export moveto="test/"
-x="${moveto: -1}"
-if [[ $x != "/" ]]; then
-  moveto="$moveto"/
-fi
-
-if [ ! -d "$moveto" ]; then
-  echo $moveto does not exist, using current directory or q to quit
-  moveto=""
-  read -p  "q to quit, anything else to continue  " q
-  if [[ $q == "q" ]]; then
-    exit
-  fi
-fi
-
-
-if [[ "$TS_AS_FILENAME" == "TRUE" ]]; then
-  echo You are renaming files with the Unix timestamp
-else
-  echo You are using the existing file name
-fi
-
-
-if [[ "$USE_LMDATE" == "TRUE" ]]; then
-  echo You are using modification date where exif data are unavailable
-else
-  echo Files with exif data will be put in a \'No exif directory\'
-fi
-
-if [[ "$USE_FILE_EXT" == "TRUE" ]]; then
-  echo The file extension will go to lower case
-else
-  echo File extensions will be ignored
-fi
-
-
-if [[ "$JPEG_TO_JPG" == "TRUE" ]]; then
-  echo File extension jpeg will be changed to jpg
-fi
-
-if [[ "$moveto" != "" ]]; then
-  echo The directories will be created at "$moveto"
-else
-  echo The directories will be created at $PWD
-fi
-
-read -p  "q to quit, anything else to continue  " q
-if [[ $q == "q" ]]; then
-  exit
-fi
-}
+MOVETO=""
 #
 ###############################################################################
 # End of settings. If you feel the need to modify anything below here, please share
-# your edits at the url above so that improvements can be made to the script. Thanks!
+# your edits at the URL above so that improvements can be made to the script. Thanks!
 #
 #
 # Assume find, grep, stat, awk, sed, tr, etc.. are already here, valid, and working.
@@ -115,84 +64,130 @@ fi
 # sorting. Minor output issue when run in this manner. Related to find -print0 below.
 #
 # Are we supposed to run an action? If not, skip this entire section.
-
 if [[ "$1" == "doAction" && "$2" != "" ]]; then
-  # Check for EXIF and process it
-  echo -n ": Checking EXIF... "
-  DATETIME=`identify -verbose "$2" | grep "exif:DateTime:" | awk '{print $2" "$3}'`
-  if [[ "$DATETIME" == "" ]]; then
-    echo "not found."
-    echo $USE_LMDATE
-    if [[ $USE_LMDATE == "TRUE" ]]; then
-      # I am deliberately not using %Y here because of the desire to display the date/time
-      # to the user, though I could avoid a lot of post-processing by using it.
-      DATETIME=`stat --printf='%y' "$2" | awk -F. '{print $1}' | sed y/-/:/`
-      echo " Using LMDATE: $DATETIME"
+    exact_duplicate=1
+    # Check for EXIF and process it
+    echo -n ": Checking EXIF... "
+
+    # DATETIME FORMAT: 2017:02:20 21:43:11
+    DATETIME=$(exiftool -S -s "-datetimeoriginal" -d "%Y:%m:%d %H:%M:%S" "$2")
+
+    # If identify fails use identify instead (only if photo)
+    if [[ "$DATETIME" == "" ]] && $(file "$2" | grep -qE 'image|bitmap'); then
+        identify -verbose "$2" 2>/dev/null| grep "exif:DateTimeOriginal" | awk -F' ' '{print $2" "$3}'
+    fi
+
+    # If identify fails b/c video file then attempt to use mediainfo
+    if [[ "$DATETIME" == "" ]] && $(file "$2" | grep -qvE 'image|bitmap'); then
+        MEDIAINFO=$(mediainfo "$2"|grep -m 1 -E 'Tagged date|Mastered date')
+
+        i=0
+        while [[ "$DATETIME" == ""  ]] ; do
+            case $i in
+            0)
+                # For "Tagged date" match
+                DATETIME=$(echo "${MEDIAINFO}" | sed -nr 's/.*([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}).*/\1:\2:\3 \4:\5:\6/p')
+                i=$((i+1))
+                ;;
+            1)
+                # For "Mastered date" match
+                # Master Date v01 -- FRI JAN 11 22:14:20 2013
+                PARSETIME=$(echo "${MEDIAINFO}" | sed -nr 's/.*([A-Z]{3}) ([A-Z]{3}) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}) ([0-9]{4}).*/\3-\2-\7 \4:\5:\6/p')
+
+                # Master Date v02 -- 2004-04-06 09:56:51 (default fallback)
+                if [[ "$PARSETIME" == "" ]]; then
+                    PARSETIME=$(echo "$MEDIAINFO" | sed 's/Mastered date.*: //g' )
+                fi
+                if [[ "$PARSETIME" != "" ]]; then
+                    DATETIME=$(date -d "$PARSETIME" '+%Y:%m:%d %H:%M:%S')
+                fi
+                i=$((i+1))
+                ;;
+            *)
+                break
+                ;;
+            esac
+        done
+        unset i
+    fi
+
+    if [[ "$DATETIME" == "" ]]; then
+        echo "not found."
+
+        if [[ $USE_LMDATE == "TRUE" ]]; then
+            # I am deliberately not using %Y here because of the desire to display the date/time
+            # to the user, though I could avoid a lot of post-processing by using it.
+            DATETIME=`stat --printf='%y' "$2" | awk -F. '{print $1}' | sed y/-/:/`
+            echo " Using LMDATE: $DATETIME"
+        else
+            echo " Moving to ./noexif/"
+            echo mkdir -p "${MOVETO}noexif" && echo mv -n "$2" "${MOVETO}noexif/"
+            mkdir -p "${MOVETO}noexif" && mv -n "$2" "${MOVETO}noexif/"
+            exit
+        fi
     else
-      echo " Moving to ./noexif/"
-      mkdir -p "${moveto}noexif" && mv -b -f "$2" "${moveto}noexif"
-      exit
-    fi;
-  else
-    echo "found: $DATETIME"
-  fi;
-  # The previous iteration of this script had a major bug which involved handling the
-  # renaming of the file when using TS_AS_FILENAME. The following sections have been
-  # rewritten to handle the action correctly as well as fix previously mangled filenames.
-  #
-  EDATE=`echo $DATETIME | awk '{print $1}'`
-  # Evaluate the file extension
-  extension=`file -b "$2" | awk -F' ' '{print $1}' | tr '[:upper:]' '[:lower:]'`
+        echo "found: $DATETIME"
+    fi
 
-  if [ "${USE_FILE_EXT}" != "TRUE" ]; then
-    # Lowercase and use the current extension as-is
-    extension=`echo "$2" | awk -F. '{print $NF}' | tr '[:upper:]' '[:lower:]'`
-  fi;
-  # Evaluate the file name
+    # The previous iteration of this script had a major bug which involved handling the
+    # renaming of the file when using TS_AS_FILENAME. The following sections have been
+    # rewritten to handle the action correctly as well as fix previously mangled filenames.
+    # FIXME: Collisions are not fully handled.
+    #
+    EDATE=`echo $DATETIME | awk -F' ' '{print $1}'`
 
-  if [[ "${extension}" == "jpeg" && "${JPEG_TO_JPG}" == "TRUE" ]]; then
-  extension="jpg";
-  fi
+    # Evaluate the correct file extension (excluding videos)
+    if [ "$USE_FILE_EXT" == "TRUE" ] && $(file "$2" | grep -qE 'image|bitmap'); then
+        # Get the FILE type and lowercase it for use as the extension
+        EXT=`file -b "$2" | awk -F' ' '{print $1}' | tr '[:upper:]' '[:lower:]'`
 
+        if [[ "${EXT}" == "jpeg" && "${JPEG_TO_JPG}" == "TRUE" ]]; then EXT="jpg"; fi;
+    else
+        # Lowercase and use the current extension as-is
+        EXT=`echo "$2" | awk -F. '{print $NF}' | tr '[:upper:]' '[:lower:]'`
+    fi
 
-  if [ "$TS_AS_FILENAME" == "TRUE" ]; then
-    # Get date and times from EXIF stamp
-    ETIME=`echo $DATETIME | awk '{print $2}'`
-    # Unix Formatted DATE and TIME - For feeding to date()
-    UFDATE=`echo $EDATE | sed y/:/-/`
-    # Unix DateSTAMP
-    UDSTAMP=`date -d "$UFDATE $ETIME" +%s`
-    echo " Will rename to $UDSTAMP.$extension"
-    filename="/$UDSTAMP.$extension"
-  else
-    y=${2%.*} # strips everything after dot
-    y=`echo $y | sed 's/^..//'`
-    directoryname=`echo $EDATE | sed y-:-/-`
-    directoryname=$directoryname/
-    filename="${y}"
-    filename=`basename $filename`
-    filename=/$filename.$extension
-  fi;
-  
-  # DIRectory NAME for the file move
-  # sed issue for y command fix provided by thomas
-  directoryname=`echo $EDATE | sed y-:-/-`
-  if [ -f ${moveto}${directoryname}${filename} ]; then
-    x=0
-    while [ -f ${moveto}${directoryname}${filename} ]; do
-      let "x+=1"
-      filename=${filename%.*}
-      filename=${filename%_*} #already a duplicate
-      filename="${filename}_$x"
-      filename="$filename.$extension"
-    done;
-    x=0
-  fi
-  echo -n " Moving to ${moveto}${directoryname}${filename} ... "
-  mkdir -p "${moveto}${directoryname}" && mv -b -f "$2" "${moveto}${directoryname}${filename}"
-  echo "done."
-  echo ""
-  exit
+    # DIRectory NAME for the file move
+    # sed issue for y command fix provided by thomas
+    DIRNAME=`echo $EDATE | sed y-:-/-`
+
+    # Evaluate the file name
+    if [ "$TS_AS_FILENAME" == "TRUE" ]; then
+        # Get date and times from EXIF stamp
+        ETIME=`echo $DATETIME | awk -F' ' '{print $2}'`
+
+        # Unix Formatted DATE and TIME - For feeding to date()
+        UFDATE=`echo $EDATE | sed y/:/-/`
+
+        # Unix DateSTAMP
+        UDSTAMP=$(date -d "$UFDATE $ETIME" +%s)
+        MVCMD="/$UDSTAMP.$EXT"
+
+        # Handle collisions
+        while [[ -e "${MOVETO}${DIRNAME}${MVCMD}" ]] ; do
+            # Check if photo is duplicate
+            if [[ $DIRNAME != "duplicates" ]] && ( $(nice -n 19 cmp "$2" "${MOVETO}${DIRNAME}${MVCMD}" >/dev/null) || ( $(file "$2" | grep -qE 'image|bitmap') && [[ "$(nice -n 19 convert "$2" "${MOVETO}${DIRNAME}${MVCMD}" -trim +repage -resize "256x256^!" -metric RMSE -format %[distortion] -compare info:)" == "0" ]] )) ; then
+                echo "INFO: Duplicate detected"
+                DIRNAME="duplicates"
+                unset i
+                continue
+            fi
+            let ${i:=-1}
+            i=$((i+1))
+            MVCMD="/$UDSTAMP-$i.$EXT"
+        done
+        unset i
+
+        echo "INFO: Will rename to $(basename $MVCMD)"
+    fi
+
+    # Fix permissions
+    chmod 644 "$2"
+
+    echo mkdir -p "${MOVETO}${DIRNAME}" && echo mv -n "$2" "${MOVETO}${DIRNAME}${MVCMD}"
+    mkdir -p "${MOVETO}${DIRNAME}" && mv -n "$2" "${MOVETO}${DIRNAME}${MVCMD}"
+    echo -e "INFO: done.\n"
+    exit
 fi;
 #
 ###############################################################################
@@ -202,31 +197,33 @@ fi;
 # parameters required to trigger the above loop to do the heavy lifting of the sorting.
 # Could probably be optimized into a function instead, but I don't think there's an
 # advantage performance-wise. Suggestions are welcome at the URL at the top.
-
-varbles
 for x in "${FILETYPES[@]}"; do
-  # Check for the presence of imagemagick and the identify command.
-  # Assuming its valid and working if found.
-  I=`which identify`
-  if [ "$I" == "" ]; then
+    # Check for the presence of imagemagick and the identify command.
+    # Assuming its valid and working if found.
+    I=`which identify`
+    if [ "$I" == "" ]; then
     echo "The 'identify' command is missing or not available."
     echo "Is imagemagick installed?"
     exit 1
-  fi;
-  echo "Scanning for $x..."
-  # FIXME: Eliminate problems with unusual characters in filenames.
-  # Currently the exec call will fail and they will be skipped.
-  #Jeff suggest maxdepth 1 to prevent multiple directories being corrupted by mistake
-  #find . -maxdepth 1 -iname "$x" -print0 -exec sh -c "$0 doAction '{}'" \;
-  find . -iname "$x" -print0 -exec sh -c "$0 doAction '{}'" \;
+    fi;
+    echo "Scanning for $x..."
 
-  echo "... end of $x"
-done;
+    # FIXME: Eliminate problems with unusual characters in filenames.
+    # Currently the exec call will fail and they will be skipped.
+    find . \( -iname "*lost*" -prune -o -regextype posix-awk -regex "./[0-9]{4}" -prune -o -iname *"noexif"* -prune -o -iname *"duplicates"* -prune -o -iname *"slideshows"* -prune -o -iname *"raw"* -prune \) -o -type f -iname "$x" -print0 -exec sh -c "$0 doAction \"{}\"" \;
+    echo "... end of $x"
+done
+
 # clean up empty directories. Find can do this easily.
 # Remove Thumbs.db first because of thumbnail caching
-echo -n "Removing Thumbs.db files ... "
+echo "INFO: Removing THM files ... "
+find . -iname '.thm' -delete
+echo "INFO: Removing Thumbs.db files ... "
 find . -name Thumbs.db -delete
-echo "done."
-echo -n "Cleaning up empty directories ... "
+echo "INFO: done."
+echo "INFO: Cleaning up empty directories ... "
 find . -empty -delete
-echo "done."
+echo "INFO: done."
+mkdir dump
+touch dump/.exclude_from_backup
+echo "INFO: Recreating dump directory ... "
